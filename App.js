62a9238e7afb55bbca6b4ea36eddbfd1638d456e
anoop <anoopkashok800@gmail.com>
2019-01-16 19:05:40 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,77 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  ScrollView,
+  Dimensions
+  } from 'react-native';
+import {createDrawerNavigator, DrawerItems} from 'react-navigation';
+import HomeScreen from './screens/HomeScreen';
+import SettingsScreen from './screens/SettingsScreen';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+      < AppDrawerNavigator />
+    );
+  }
+}
+
+const CustomDrawerComponent= (props)=>(
+  <SafeAreaView>
+    <View style={{height:150,backgroundColor:'black'}}>
+    </View>
+    <ScrollView>
+      <DrawerItems {...props}/>
+    </ScrollView>
+  </SafeAreaView>
+
+)
+
+const AppDrawerNavigator = createDrawerNavigator({
+  Home:HomeScreen,
+  Settings:SettingsScreen
+
+},
+{
+  contentComponent: CustomDrawerComponent
+
+})
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
---
+++ screens/HomeScreen.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet
+} from 'react-native';
+
+
+class HomeScreen extends Component{
+  render(){
+
+    return(
+      <View style={styles.container}>
+        <Text> HomeScreen </Text>
+      </View>
+    );
+
+  }
+}
+
+export default HomeScreen;
+
+const styles ={
+  container:{
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+};
