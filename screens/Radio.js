6527b511592ab16f98a4ab2c889d4751b51eb01e
anoop <anoopkashok800@gmail.com>
2019-01-24 18:48:41 +0530

    Radio UI updated

--- screens/Radio.js
+++ screens/Radio.js
@@ -25,6 +25,8 @@ import {
 import { DrawerActions } from 'react-navigation';
 import { ReactNativeAudioStreaming } from 'react-native-audio-stream';
 import { Player } from 'react-native-audio-stream';
+import playImage from '../assets/play.png';
+import pauseImage from '../assets/pause.png';
 
 
 //const audioLiveStreamURL = "https://indemandradio.com/in_demand_radio"
@@ -47,53 +49,55 @@ class Radio extends Component{
 
       dataSource:[],
       playButtonTitle: "Play",
-      nowArtist:"",
+      NowArtist:"",
       NowTitle:"",
       NowArtistImage:"",
       NowShowName:"",
       NowShowDescription:"",
-      NowShowImage:""
+      NowShowImage:"",
+      isPlaying: false
 
     }
   }
 
   onPlayButtonPress = () => {
 
-      if(this.state.playButtonTitle === "Play"){
-        this.setState({playButtonTitle: 'Pause'})
-        ReactNativeAudioStreaming.play("https://indemandradio.com/in_demand_radio", {showIniOSMediaCenter: true});
-      }else {
-        this.setState({playButtonTitle: 'Play'})
+      if(this.state.isPlaying){
+        this.setState({isPlaying: false})
         ReactNativeAudioStreaming.pause();
+      }else {
+        this.setState({isPlaying: true})
+        ReactNativeAudioStreaming.play("https://indemandradio.com/in_demand_radio", {showIniOSMediaCenter: true});
       }
+
+  }
+
+  getAirTime(){
+    return "11:30";
   }
 
   renderItem = ({item}) => {
 
     return(
       <SafeAreaView>
-      <TouchableWithoutFeedback onPress={() => this.actionOnRow(item)}>
         <View style = {styles.listConatainer}>
           <Image
           style = {styles.listImageStyle}
-          source = {{uri: item.Image}}
+          source={{ uri: item.Image}}
+          onError={(e) => { this.props.source = { reqire: '../assets/placeholder.png'}}}
           />
           <View style = {styles.listContentStyle}>
-            <Text style={styles.listMainTitleStyle}>{item.Title}</Text>
-            <Text style={styles.listSubTitleStyle}>{item.Artist}</Text>
+            <Text style={styles.listMainTitleStyle}>{item.Artist}</Text>
+            <Text style={styles.listSubTitleStyle}>{item.Title}</Text>
             <Text style={styles.listSubTitleStyle}>{item.AirTime}</Text>
           </View>
         </View>
-      </TouchableWithoutFeedback>
       </SafeAreaView>
     )
 
   }
 
-  actionOnRow(item) {
-   console.log('Selected Item :',item);
 
-  }
 
   componentDidMount(){
     const url = "https://api.indemandradio.com/metadata"
@@ -101,7 +105,22 @@ class Radio extends Component{
       .then((response) => response.json())
       .then((responseJson)=>{
        this.setState({
-         dataSource:responseJson.PlayHistory
+         dataSource:responseJson.PlayHistory,
+         NowArtist:responseJson.NowArtist,
+         NowTitle:responseJson.NowTitle,
+         NowArtistImage:responseJson.NowArtistImage,
+         NowShowName:responseJson.NowShowName,
+         NowShowDescription:responseJson.NowShowDescription,
+         NowShowImage:responseJson.NowShowImage
+       }),
+       this.setState({
+         NowArtist:responseJson.NowArtist
+       }),
+       this.setState({
+         NowTitle:responseJson.NowTitle
+       }),
+       this.setState({
+         NowArtistImage:responseJson.NowArtistImage
        })
     })
     .catch((error)=>{
@@ -109,29 +128,47 @@ class Radio extends Component{
     })
   }
 
+  //<Player url={"https://indemandradio.com/in_demand_radio"} />
+
   render(){
     return(
       <Container>
-      <Header>
-        <Left>
+
+        <Header>
+          <Left>
           <Icon name="ios-menu" onPress={() => this.props.navigation.openDrawer()}> </Icon>
-        </Left>
-        <Body><Title>Radio</Title></Body>
-        <Right />
-      </Header>
+          </Left>
+          <Body><Title>Now playing</Title></Body>
+          <Right />
+        </Header>
+
         <View style={styles.PlayerViewStyle}>
-          <Player url={"https://indemandradio.com/in_demand_radio"} />
+            <Image
+            style = {styles.NowPlayingImageStyle}
+            source = {{uri: this.state.NowArtistImage}}
+            />
+            <View style = {styles.listContentStyle}>
+              <Text style={styles.NowPlayingTitleStyle}>{this.state.NowArtist}</Text>
+              <Text style={styles.listSubTitleStyle}>{this.state.NowTitle}</Text>
+            </View>
+            <TouchableOpacity onPress={() => this.onPlayButtonPress()}>
+              <Image
+              style = {styles.NowPlayingImageStyle}
+              source={this.state.isPlaying === true ?require('../assets/pause.png'):require('../assets/play.png')}
+              />
+            </TouchableOpacity>
         </View>
-        <View>
-          <Text>Recently played</Text>
+
+        <View style= {styles.RecentlyPlayedViewStyle}>
+          <Text style= {styles.RecentlyPlayedTitleStyle}>Recently played</Text>
         </View>
 
         <Content contentContainerStyle={styles.container}>
-        <FlatList
+          <FlatList
           data={this.state.dataSource.reverse()}
           renderItem={this.renderItem}
           keyExtractor={(item, index) => index.toString()}
-        />
+          />
         </Content>
       </Container>
     );
@@ -145,6 +182,24 @@ const styles ={
   container:{
     flex: 1,
   },
+  RecentlyPlayedTitleStyle:{
+    fontSize:20,
+    color:'white',
+    marginLeft: 5,
+    fontWeight:'bold'
+  },
+  NowPlayingTitleStyle:{
+    fontSize:25,
+    color:'#ca4587',
+    fontWeight:'bold'
+  },
+  NowPlayingArtistTextStyle:{
+
+  },
+  RecentlyPlayedViewStyle:{
+    backgroundColor:'#313030',
+    height:25
+  },
   listConatainer:{
     flex:1,
     flexDirection:'row',
@@ -154,23 +209,37 @@ const styles ={
   },
   listContentStyle:{
     flex:1,
-    justifyContent:'center'
+    justifyContent:'flex-start',
+    marginLeft:5
   },
   listImageStyle:{
     width:100,
     height:100,
     margin:5
   },
+  NowPlayingImageStyle:{
+    width:100,
+    height:100,
+    margin:5
+
+  },
   listMainTitleStyle:{
     fontSize: 18,
-    color:'black',
+    color:'#ca4587',
+    fontWeight:'bold'
   },
   listSubTitleStyle:{
     fontSize:14,
     color:'gray'
   },
   PlayerViewStyle:{
-    backgroundColor:'white',
-    height: 150
+    backgroundColor:'#313030',
+    height: 110,
+    flexDirection:'row',
+    alignItem: 'center'
+  },
+  NowPlayingViewStyle:{
+    backgroundColor: 'black',
+    height:180
   }
 };
